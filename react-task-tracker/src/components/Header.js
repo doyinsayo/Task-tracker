7558842c3a950819df8d0991f1e6cddbfd5934bf
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2021-01-21 15:02:18 +0100

    buttons
    buttons,events,props,propTypes

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+          <header className='header'>
             <h1>{title}</h1>
+            <Button color ='green' text='Add' />
         </header>
+        
     )
 }
 
@@ -16,5 +19,10 @@ Header.defaultProps = {
 Header.propTypes = {
     title : PropTypes.string.isRequired,
 }
+
+//CSS IN JS
+//const headingStyle = {
+ //   color:'red', backgroundColor: 'black'
+//}
 
 export default Header;
